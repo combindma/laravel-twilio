a491fc36e05085d5c1faaf8a5e7a0c8579769f32
Combind <webmaster@combind.ma>
2023-03-18 17:25:04 +0100

    Added support for Laravel 10

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,14 @@
 
 namespace Combindma\Twilio\Tests;
 
+use Combindma\Twilio\Twilio;
 use Combindma\Twilio\TwilioServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
+    public Twilio $twilio;
+
     protected function getPackageProviders($app): array
     {
         return [
@@ -16,12 +19,10 @@ class TestCase extends Orchestra
 
     public function getEnvironmentSetUp($app)
     {
-        config()->set('database.default', 'testing');
-        config()->set('database.connections.testing', [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-            'prefix' => '',
-        ]);
-        config()->set('app.locale', 'fr');
+        $app['config']->set('twilio.sid', 'twilio_sid');
+        $app['config']->set('twilio.token', 'twilio_token');
+        $app['config']->set('twilio.from', '0666666666');
+        $app['config']->set('twilio.enabled', true);
+        $this->twilio = new Twilio();
     }
 }
--- tests/TwilioTest.php
+++ tests/TwilioTest.php
@@ -1,5 +1,8 @@
 <?php
 
-it('can send an sms to recipient', function () {
-    //TODO
+it('can test if config file are set', function () {
+    expect($this->twilio->sid())->toBe('twilio_sid')
+        ->and($this->twilio->token())->toBe('twilio_token')
+        ->and($this->twilio->from())->toBe('0666666666')
+        ->and($this->twilio->isEnabled())->toBeTrue();
 });
